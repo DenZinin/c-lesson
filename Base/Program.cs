8a1b31b4dc2d806a4f7af8326bcd3ae6c85369a1
DenisZ <zinindenis2@gmail.com>
2023-01-21 16:38:50 +0300

    split lesson

---
+++ Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 4;
+int b = 8;
+int c = 12;
+int d = 6;
+int e = 10;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine(max);
