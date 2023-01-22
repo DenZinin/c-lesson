d86f3c36cc4f8ed8d76ef0d2784c3ae437b336e2
DenisZ <zinindenis2@gmail.com>
2023-01-22 19:05:50 +0300

    add seminar 3

---
+++ funcLesson8/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
+int Prompt(string msg)
+{
+    System.Console.WriteLine($"{msg} : ");
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+int Factorial(int n)
+{
+    int prod = 1;
+    for (int i = 1; i<=n; i++)
+    {
+        prod = prod * i;
+    }
+    return prod;
+    
+}
+
+int usernumber = Prompt("Введите число: ");
+
+int res = Factorial(usernumber);
+
+Console.WriteLine($"Факториал числа {usernumber} равен {res}");
